f131c649b143f65e04ceb7717fa76dd768ff7680
kdoyle39 <kaley_doyle@brown.edu>
2024-04-04 19:48:10 -0400

    final changes

--- Analysis/data.R
+++ Analysis/data.R
@@ -1,17 +1,5 @@
 ### Data taken from the World Inequality Database
 
-# Load the necessary packages
-library(readr)
-library(tidyr)
-install.packages("WDI")
-library(WDI)
-library(tidyverse)
-library(readxl) #for excel files
-install.packages("devtools")
-devtools::install_github("WIDworld/wid-r-tool")
-library(wid)
-library(dplyr)
-
 url_summary <- "https://wir2022.wid.world/www-site/uploads/2022/03/WIR2022TablesFigures-Summary.xlsx"
 download.file(url = url_summary, destfile = "./data/WIR2022s.xlsx", mode = "wb") 
 
@@ -19,12 +7,12 @@ excel_sheets("./data/WIR2022s.xlsx")
 
 #download data for wealth/income ratio in the US
 
-##net public wealth to net national income ratio
-wwealg <- download_wid(indicators = "wwealg", areas = "US", years = "all")
-##net private wealth to net national income ratio
-wwealp <- download_wid(indicators = "wwealp", areas = "US", years = "all")
-##net personal wealth to net national income ratio
-wwealh <- download_wid(indicators = "wwealh", areas = "US", years = "all")
+    ##net public wealth to net national income ratio
+    wwealg <- download_wid(indicators = "wwealg", areas = "US", years = "all")
+    ##net private wealth to net national income ratio
+    wwealp <- download_wid(indicators = "wwealp", areas = "US", years = "all")
+    ##net personal wealth to net national income ratio
+    wwealh <- download_wid(indicators = "wwealh", areas = "US", years = "all")
 
 ##create variable names
 public<- wwealg %>% select(country, year, public = value)
